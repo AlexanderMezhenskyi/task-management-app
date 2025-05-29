ad204dffc61fe7b802c50835a4589c885cb0b494
Oleksandr Mezhenskyi <alexmezhenskyi@gmail.com>
2025-05-29 22:53:02 +0200

    Updates

--- assets/ProjectsView-Cwa8BMjJ.js
+++
@@ -1,1 +0,0 @@
-import{P as e}from"./ProjectList-zGnB9rgC.js";import{d as o,f as t,o as r}from"./index-D3OWCAS7.js";import"./helpers-DwjziIZx.js";const _=o({__name:"ProjectsView",setup(s){return(a,c)=>(r(),t(e))}});export{_ as default};
